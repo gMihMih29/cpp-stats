922eadf2d6d67d4b7c065cb50fb5cd580eb4d4f7
Mikhail Getmanov <mmgetmanov@gmail.com>
2025-02-02 11:00:59 +0300

    feat test cpp_stats: added tests for NUMBER_OF_C_C++_FILES

--- tests/test_file_sieve.py
+++ tests/test_file_sieve.py
@@ -3,77 +3,10 @@ Tests module file_sieve.py
 '''
 
 from pathlib import Path
-import pytest
+
+from utils.asserts import assert_lists_equal_unordered
 
 import cpp_stats.file_sieve
-
-def assert_lists_equal_unordered(expected, actual):
-    '''
-    Asserts that two lists contain same elements.
-    
-    Parameters:
-    `expected` (list): Expected list.
-    `actual` (list): List to check.
-    '''
-    assert sorted(expected) == sorted(actual), f"Expected: {expected}, Actual: {actual}"
-
-def rename_git_ignore_and_modules(repo_path: Path, to_test: bool = True):
-    '''
-    Renames `test.gitignore` to `.gitignore` and 
-    `test.gitmodules` to `.gitmodules` if `to_test == False` or
-    renames `.gitignore` to `test.gitignore` and 
-    `.gitmodules` to `test.gitmodules` if `to_test == True`
-    '''
-    if to_test:
-        for entry in repo_path.rglob('.gitignore'):
-            entry.rename(entry.parent.joinpath('test.gitignore'))
-        for entry in repo_path.rglob('.gitmodules'):
-            entry.rename(entry.parent.joinpath('test.gitmodules'))
-    else:
-        for entry in repo_path.rglob('test.gitignore'):
-            entry.rename(entry.parent.joinpath('.gitignore'))
-        for entry in repo_path.rglob('test.gitmodules'):
-            entry.rename(entry.parent.joinpath('.gitmodules'))
-
-@pytest.fixture
-def repo():
-    '''
-    Returns path to test repository located at `/tests/data/repo`
-    '''
-    repo_path = Path('./tests/data/repo')
-    rename_git_ignore_and_modules(repo_path, False)
-    yield repo_path
-    rename_git_ignore_and_modules(repo_path, True)
-
-@pytest.fixture
-def repo_with_gitignore():
-    '''
-    Returns path to test repository located at `/tests/data/repo_with_gitignore`
-    '''
-    repo_path = Path('./tests/data/repo_with_gitignore')
-    rename_git_ignore_and_modules(repo_path, False)
-    yield repo_path
-    rename_git_ignore_and_modules(repo_path, True)
-
-@pytest.fixture
-def repo_with_2gitignore():
-    '''
-    Returns path to test repository located at `/tests/data/repo_with_2gitignore`
-    '''
-    repo_path = Path('./tests/data/repo_with_2gitignore')
-    rename_git_ignore_and_modules(repo_path, False)
-    yield repo_path
-    rename_git_ignore_and_modules(repo_path, True)
-
-@pytest.fixture
-def repo_with_ignr_modules():
-    '''
-    Returns path to test repository located at `/tests/data/repo_with_ignr_modules`
-    '''
-    repo_path = Path('./tests/data/repo_with_ignr_modules')
-    rename_git_ignore_and_modules(repo_path, False)
-    yield repo_path
-    rename_git_ignore_and_modules(repo_path, True)
 
 def test_find_gitignore_1(repo: Path):
     '''
