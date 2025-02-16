7cea2b9271cf1e17fbabd3a41f0e74495f072837
Mikhail Getmanov <mmgetmanov@gmail.com>
2025-02-16 21:00:03 +0300

    feat metric structure: each metric will have own metric class derived from Metic

--- src/cpp_stats/metrics/lines_of_code.py
+++ src/cpp_stats/metrics/lines_of_code.py
@@ -3,11 +3,37 @@ from pathlib import Path
 from cpp_stats.metrics.metric_calculator import BasicMetricCalculator
 from cpp_stats.metrics.metric import Metric
 
+METRIC_NAME = 'LINES_OF_CODE'
+
+class LinesOfCodeMetric(Metric):
+    '''
+    Represents LINES_OF_CODE metric.
+    '''
+    def __init__(self, value: float):
+        '''
+        Initializes metric.
+        
+        Parameters:
+        value (float): Metric value.
+        '''
+        self.value = value
+
+    def __add__(self, other):
+        if not isinstance(other, LinesOfCodeMetric):
+            return NotImplemented
+        return LinesOfCodeMetric(self.value + other.value)
+
+    def get(self) -> tuple[str, float]:
+        '''
+        Returns metric value.
+        '''
+        return METRIC_NAME, self.value
+
+
 class LinesOfCodeCalculator(BasicMetricCalculator):
     '''
     Calculates LINES_OF_CODE.
     '''
-    CALCULATED_METRIC_NAME = 'LINES_OF_CODE'
 
     def __call__(self, file_paths: list[Path]) -> list[Metric]:
         cnt = 0
@@ -15,5 +41,5 @@ class LinesOfCodeCalculator(BasicMetricCalculator):
             with open(file_path, 'r') as f:
                 while f.readline():
                     cnt += 1
-        return Metric(LinesOfCodeCalculator.CALCULATED_METRIC_NAME, cnt)
+        return LinesOfCodeMetric(cnt)
                 
--- src/cpp_stats/metrics/metric.py
+++ src/cpp_stats/metrics/metric.py
@@ -3,28 +3,12 @@ class Metric:
     Class that stores metric name and value.
     '''
 
-    def __init__(self, name: str, value: float, count: int = 1):
-        '''
-        Initializes metric.
-        
-        Parameters:
-        name (str): Metric name.
-        value (float): Metric value.
-        '''
-        self.name = name
-        self.value = value
-        self.count = count
-
     def __add__(self, other):
-        if not isinstance(other, Metric):
-            return NotImplemented
-        if self.name != other.name:
-            return NotImplemented
-        return Metric(self.name, self.value + other.value, self.count + other.count)
+        pass
 
     def get(self):
         '''
         Returns metric value. Should be called since metric can be summarization of many files,
         namespaces, classes and etc.
         '''
-        return self.value / self.count
+        pass
