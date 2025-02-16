ed30902bd2cd43f2d3408a3a38fb562427762030
Mikhail Getmanov <mmgetmanov@gmail.com>
2025-02-16 21:43:46 +0300

    feat metric_calculator: removed list[Metric] from LOC calculator

--- tests/test_lines_of_code.py
+++ tests/test_lines_of_code.py
@@ -1,3 +1,17 @@
 '''
 Tests module lines_of_code.py
 '''
+from pathlib import Path
+
+from cpp_stats.metrics.lines_of_code import LinesOfCodeCalculator, LinesOfCodeMetric
+
+def test_regular_1():
+    files = [
+        Path('./tests/data/repo/main.cpp')
+    ]
+    calculator = LinesOfCodeCalculator()
+    expected = 6
+
+    _, actual = calculator(files).get()
+
+    assert expected == actual , f"Expected: {expected}, Actual: {actual}"
