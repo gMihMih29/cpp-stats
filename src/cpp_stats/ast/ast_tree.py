ff494b39a4a86dac96b5e29982d1599d8baf8109
Mikhail Getmanov <mmgetmanov@gmail.com>
2025-03-06 17:14:28 +0300

    feat ast: added ast traversal with integrated calculations

--- src/cpp_stats/ast/ast_tree.py
+++ src/cpp_stats/ast/ast_tree.py
@@ -6,6 +6,7 @@ from pathlib import Path
 
 import clang.cindex
 
+from cpp_stats.metrics.metric_calculator import ClangMetricCalculator, Metric
 from cpp_stats.ast.language import LanguageContruct, Namespace
 
 def get_ast_tree(c_cxx_files: list[Path]) -> Namespace:
@@ -43,3 +44,44 @@ def _parse_children(
             cur = cur.semantic_parent
         global_namespace.add_construct(semantic_parents_stack)
         _parse_children(global_namespace, child, analyzed_file)
+
+def analyze_ast(c_cxx_files: list[Path], calculators: dict[str, ClangMetricCalculator]) -> dict[str, Metric]:
+    '''
+    Analyzes ast based on `c_cxx_files` and calculates metrics using `calculators`
+    
+    Parameters:
+    `c_cxx_files` (`list[Path]`): C/C++ files to parse.
+    `calculators` (`list[ClangMetricCalculator]`): calculators to calculate metris.
+    
+    Returns:
+    `dict[str, Metric]`: dictionary with all metrics calculated by `calculators`.
+    '''
+    clang.cindex.Config.set_library_file('D:\\Program_Files\\mingw64\\bin\\libclang.dll')
+    index = clang.cindex.Index.create()
+    result = dict()
+    for i, file_path in enumerate(c_cxx_files):
+        translation_unit = index.parse(file_path, args=['-x', 'c++'])
+        _analyze_children(
+            result,
+            calculators,
+            translation_unit.cursor,
+            str(file_path.resolve())
+        )
+    return result
+
+def _analyze_children(
+    result: dict[str, Metric],
+    calculators: dict[str, ClangMetricCalculator],
+    cursor: clang.cindex.Cursor,
+    analyzed_file: str
+    ):
+    for child in cursor.get_children():
+        if child.location.file is None or child.location.file.name != analyzed_file:
+            continue
+        for clc in calculators.items():
+            metric = clc[1](child)
+            if result.get(metric.name(), None) is None:
+                result[metric.name()] = metric
+            else:
+                result[metric.name()] += metric
+        _analyze_children(result, calculators, child, analyzed_file)
