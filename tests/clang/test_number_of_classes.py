6e4e886bd3987b811411a4fd433e2161c7387b0b
Mikhail Getmanov <mmgetmanov@gmail.com>
2025-03-10 12:38:14 +0300

    feat tests: added support of clang inside tests

--- tests/clang/test_number_of_classes.py
+++ tests/clang/test_number_of_classes.py
@@ -1,8 +1,11 @@
 import pytest
 
+from cpp_stats.metrics.metric_calculator import Metric
 from cpp_stats.metrics.number_of_classes import NumberOfClassesCalculator, NumberOfClassesMetric
 
-def test_when_two_equal_classes_then_ignore_one():
+import utils.clang
+
+def test_when_add_two_noc_metrics_then_sum_values():
     m1 = NumberOfClassesMetric(1)
     m2 = NumberOfClassesMetric(2)
     expected = 3
@@ -10,3 +13,17 @@ def test_when_two_equal_classes_then_ignore_one():
     _, actual = (m1 + m2).get()
 
     assert expected == actual
+
+def test_when_sum_with_unknown_metric_then_not_implemented():
+    m1 = NumberOfClassesMetric(1)
+    m2 = Metric("some metric")
+
+    with pytest.raises(NotImplementedError):
+        m1 + m2
+
+@pytest.mark.skipif(
+    (utils.clang.clang_index() is None),
+    reason="Clang cannot be found using env variable LIBCLANG_LIBRARY_PATH"
+)
+def test_find_3_classes(clang_index):
+    assert clang_index is not None
