894aa71a24878b7e0711a3e86af693b9a64cbb98
Mikhail Getmanov <mmgetmanov@gmail.com>
2025-03-10 12:55:09 +0300

    feat number of classes: added simple tests

--- src/cpp_stats/metrics/number_of_classes.py
+++ src/cpp_stats/metrics/number_of_classes.py
@@ -40,7 +40,7 @@ class NumberOfClassesCalculator(ClangMetricCalculator):
     '''
 
     def __call__(self, node: clang.cindex.Cursor) -> Metric:
-        if node.kind != clang.cindex.CursorKind.CLASS_DECL or node.is_definition():
+        if node.kind != clang.cindex.CursorKind.CLASS_DECL or not node.is_definition():
             return NumberOfClassesMetric(0)
         # for _ in node.get_children():
         #     return NumberOfClassesMetric(0)
--- tests/clang/test_number_of_classes.py
+++ tests/clang/test_number_of_classes.py
@@ -4,6 +4,7 @@ from cpp_stats.metrics.metric_calculator import Metric
 from cpp_stats.metrics.number_of_classes import NumberOfClassesCalculator, NumberOfClassesMetric
 
 import utils.clang
+import clang.cindex
 
 def test_when_add_two_noc_metrics_then_sum_values():
     m1 = NumberOfClassesMetric(1)
@@ -25,5 +26,40 @@ def test_when_sum_with_unknown_metric_then_not_implemented():
     (utils.clang.clang_index() is None),
     reason="Clang cannot be found using env variable LIBCLANG_LIBRARY_PATH"
 )
-def test_find_3_classes(clang_index):
-    assert clang_index is not None
+def test_when_definition_then_assert_class(clang_index: clang.cindex.Index):
+    calculator = NumberOfClassesCalculator()
+    tu = clang_index.parse(".\\tests\\data\\classes\\definition.hpp", args=['-x', 'c++'])
+    c1_class = None
+    for child in tu.cursor.get_children():
+        if child.kind == clang.cindex.CursorKind.CLASS_DECL and child.displayname == "C1":
+            c1_class = child
+            break
+    if c1_class is None:
+        assert False
+    expected = 1
+
+    metric = calculator(c1_class)
+
+    _, actual = metric.get()
+    assert expected == actual
+
+@pytest.mark.skipif(
+    (utils.clang.clang_index() is None),
+    reason="Clang cannot be found using env variable LIBCLANG_LIBRARY_PATH"
+)
+def test_when_declaration_then_assert_not_class(clang_index: clang.cindex.Index):
+    calculator = NumberOfClassesCalculator()
+    tu = clang_index.parse(".\\tests\\data\\classes\\declaration.hpp", args=['-x', 'c++'])
+    c1_class = None
+    for child in tu.cursor.get_children():
+        if child.kind == clang.cindex.CursorKind.CLASS_DECL and child.displayname == "C1":
+            c1_class = child
+            break
+    if c1_class is None:
+        assert False
+    expected = 0
+
+    metric = calculator(c1_class)
+
+    _, actual = metric.get()
+    assert expected == actual
