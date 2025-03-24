c3ecc632d2c795e3c83fb8271fb85e1299d49fb2
Mikhail Getmanov <mmgetmanov@gmail.com>
2025-03-24 14:23:02 +0300

    fix pylint: add docstring for function

--- src/cpp_stats/metrics/utils.py
+++ src/cpp_stats/metrics/utils.py
@@ -5,6 +5,15 @@ Module for helper functions
 import clang.cindex
 
 def mangle_cursor_name(node: clang.cindex.Cursor) -> str:
+    '''
+    Returns string that contains full name of node with parent classes, namespaces and etc.
+    
+    Example:
+    `::ThreeDRenderer::Camera`
+    
+    Parameters:
+    node (clang.cindex.Cursor): cursor for which mangled name is being calculated.
+    '''
     if node.kind == clang.cindex.CursorKind.TRANSLATION_UNIT:
         return ""
     return mangle_cursor_name(node.semantic_parent) + '::' + node.displayname
