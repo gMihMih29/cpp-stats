94253f764e29d6af324def9b289260ebbebd8519
Mikhail Getmanov <mmgetmanov@gmail.com>
2025-05-02 20:23:55 +0300

    fix pylint

--- src/cpp_stats/metrics/lcom/tcc_lcc/base.py
+++ src/cpp_stats/metrics/lcom/tcc_lcc/base.py
@@ -1,20 +1,20 @@
+'''
+Contains base logic for TCC and LCC calculations.
+'''
+
 from cpp_stats.metrics.lcom.base import LCOMClassData
 
-def calculate_connections(data: LCOMClassData) -> tuple[int, int, int]:
+def __get_direct_connections(data: LCOMClassData) -> (set[str], dict[str, set[str]]):
     '''
-    Returns NP, NDC and NIC for TCC and LCC metrics. 
+    Returns set and dictionary of direct connections in class.
     '''
-    n = len(data.method_data)
-    np = n * (n - 1) / 2
     direct_connections = {}
+    for field in data.fields:
+        direct_connections[field] = set([])
     for method_name, method_data in data.method_data.items():
         for field in method_data.used_fields:
-            if field in direct_connections:
-                direct_connections[field].add(method_name)
-            else:
-                direct_connections[field] = set([method_name])
+            direct_connections[field].add(method_name)
 
-    ndc = 0
     direct_connections_set = set([])
     for _, methods in direct_connections.items():
         for lhv in methods:
@@ -22,25 +22,43 @@ def calculate_connections(data: LCOMClassData) -> tuple[int, int, int]:
                 if lhv == rhv:
                     continue
                 direct_connections_set.add((min(lhv, rhv), max(lhv, rhv)))
-    ndc = len(direct_connections_set)
+    return direct_connections_set, direct_connections
 
+def __get_indirect_connections(
+    data: LCOMClassData,
+    direct_connections: dict[str, set[str]]) -> set[str]:
+    '''
+    Returns set of indirect connections in class.
+    '''
     indirect_connections = set([])
     for field_lhv in data.fields:
         for field_rhv in data.fields:
-            if field_lhv == field_rhv:
+            if (field_lhv == field_rhv
+                or field_lhv not in direct_connections
+                or field_rhv not in direct_connections):
                 continue
-            if field_lhv not in direct_connections or field_rhv not in direct_connections:
-                continue
-            lhv_set = direct_connections[field_lhv]
-            rhv_set = direct_connections[field_rhv]
+            lhv_set = direct_connections[field_lhv].copy()
+            rhv_set = direct_connections[field_rhv].copy()
             intersection = lhv_set.intersection(rhv_set)
             if len(intersection) == 0:
                 continue
-            lhv = lhv_set - intersection
-            rhv = rhv_set - intersection
-            for l_method in lhv:
-                for r_method in rhv:
-                    indirect_connections.add((min(l_method, r_method), max(l_method, r_method)))
-    nic = len(indirect_connections)
+            lhv_set = lhv_set - intersection
+            rhv_set = rhv_set - intersection
+            for lhv in lhv_set:
+                for rhv in rhv_set:
+                    indirect_connections.add((min(lhv, rhv), max(lhv, rhv)))
+    return indirect_connections
 
-    return (np, ndc, nic)
+def calculate_connections(data: LCOMClassData) -> tuple[int, int, int]:
+    '''
+    Returns NP, NDC and NIC for TCC and LCC metrics. 
+    '''
+    direct_connection_set, direct_connection_dict = __get_direct_connections(data)
+
+    indirect_connections = __get_indirect_connections(data, direct_connection_dict)
+
+    return (
+        len(data.method_data) * (len(data.method_data) - 1) / 2,
+        len(direct_connection_set),
+        len(indirect_connections)
+    )
